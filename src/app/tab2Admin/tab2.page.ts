1db751c8553b23a1d869f96f295e8c2ece784ebf
Jaffar Ashoor <j3ffarm@gmail.com>
2023-05-09 20:45:57 +0300

    admin can approve test drives

--- src/app/tab2Admin/tab2.page.ts
+++ src/app/tab2Admin/tab2.page.ts
@@ -1,9 +1,24 @@
-import { CarService, Car } from '../car.service';
 import { Component } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection, doc, getFirestore, updateDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {}
+export class Tab2Page {
+  db: Firestore;
+  testDrives: any;
+
+  status(tid: string, status: string) {
+    updateDoc(doc(this.db, 'testDrive', tid), { status: status });
+  }
+
+  constructor() {
+    this.db = getFirestore();
+    this.testDrives = collectionData(collection(this.db, 'testDrive'), {
+      idField: 'id',
+    });
+  }
+}
