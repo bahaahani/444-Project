632102d94e62585be28cab696ed49361d85d8e2f
Jaffar Ashoor <j3ffarm@gmail.com>
2023-05-09 13:54:29 +0300

    user can only have 3 test drives with no conflicts

--- src/app/tab1/showroom/showroom.page.ts
+++ src/app/tab1/showroom/showroom.page.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CarService, Car, Cars, TestDrive } from '../../car.service';
 import { ActivatedRoute } from '@angular/router';
-import { IonModal } from '@ionic/angular';
+import { AlertController, IonModal } from '@ionic/angular';
+import {
+  collection,
+  getCountFromServer,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from 'firebase/firestore';
 @Component({
   selector: 'app-showroom',
   templateUrl: './showroom.page.html',
@@ -9,10 +17,14 @@ import { IonModal } from '@ionic/angular';
 })
 export class ShowroomPage implements OnInit {
   public carList: Car[] = [];
-  Usr='View';
+  Usr = 'View';
   shoowid: any;
   testDriveDate = new Date();
-  constructor(public dataSrv: CarService, private route: ActivatedRoute) {
+  constructor(
+    public dataSrv: CarService,
+    private route: ActivatedRoute,
+    public alertCtrl: AlertController
+  ) {
     this.shoowid = this.route.snapshot.paramMap.get('id');
   }
 
@@ -21,15 +33,60 @@ export class ShowroomPage implements OnInit {
     this.testDriveDateModal.present();
   }
   selected_car: Cars = {} as any;
-  requestTestDrive(e: any) {
+  async requestTestDrive(e: any) {
+    let uid = this.dataSrv.getUid();
+    const requested_date = new Date(e.detail.value);
+
+    // ensure user have less than 3 test drives
+    const fb = getFirestore();
+    const testDriveCollection = collection(fb, 'testDrive');
+    const filtered_query = query(testDriveCollection, where('user', '==', uid));
+    const snapshot = await getCountFromServer(filtered_query);
+    const count = snapshot.data().count;
+    if (count >= 3) {
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'Maximum number of test drives is 3',
+        buttons: ['Dismiss'],
+      });
+      await alert.present();
+      return;
+    }
+
+    // ensure no time conflict
+    const booked_dates: Array<String> = [];
+    const date_query = await getDocs(
+      query(testDriveCollection, where('car', '==', this.selected_car.id))
+    );
+    date_query.forEach((doc) =>
+      booked_dates.push(doc.data()['date'].toDate().toDateString())
+    );
+    const request_date_string = requested_date.toDateString();
+    if (booked_dates.includes(request_date_string)) {
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'Car already booked on that day',
+        buttons: ['Dismiss'],
+      });
+      await alert.present();
+      return;
+    }
+
+    // put the test drive in firebase
     let testDrive: TestDrive = {
       car: this.selected_car.id!,
-      user: this.dataSrv.getUid(),
+      user: uid,
       carModel: this.selected_car.model,
       status: 'pending',
-      date: new Date(e.detail.value),
+      date: requested_date,
     };
     this.dataSrv.testDriveCollectionRef.doc().set(testDrive);
+    const alert = await this.alertCtrl.create({
+      header: 'Success',
+      message: 'Car booked successfully',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
   ngOnInit() {
