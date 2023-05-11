066ec75b7d783b39e441fed438f09c31d463020b
Jaffar Ashoor <j3ffarm@gmail.com>
2023-05-11 19:56:17 +0300

    user can filter cars

--- src/app/tab1/showroom/showroom.module.ts
+++ src/app/tab1/showroom/showroom.module.ts
@@ -7,9 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { ShowroomPageRoutingModule } from './showroom-routing.module';
 
 import { ShowroomPage } from './showroom.page';
+import { filterCarPipe } from 'src/app/filterCar.pipe';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ShowroomPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ShowroomPageRoutingModule,
+    filterCarPipe,
+  ],
   declarations: [ShowroomPage],
 })
 export class ShowroomPageModule {}
--- src/app/tab1/showroom/showroom.page.ts
+++ src/app/tab1/showroom/showroom.page.ts
@@ -23,12 +23,17 @@ import { Observable } from 'rxjs';
   styleUrls: ['./showroom.page.scss'],
 })
 export class ShowroomPage {
-  cars: Observable<Cars[]>;
-
   admin = this.dataSrv.admin;
+  cars: Observable<Cars[]>;
   Usr = 'View';
   shoowid: any;
   testDriveDate = new Date();
+  filterParam: any = {};
+
+  filter() {
+    this.filterParam = JSON.parse(JSON.stringify(this.filterParam));
+  }
+
   constructor(
     public dataSrv: CarService,
     private route: ActivatedRoute,
@@ -148,83 +153,4 @@ export class ShowroomPage {
     });
     modal.present();
   }
-
-  searchResults: Cars[] = [];
-  search() {
-    this.searchResults = this.dataSrv.carList.filter(
-      (car: {
-        type: string;
-        manufacturer: string;
-        model: string;
-        color: string;
-        mileage: number;
-        price: number;
-      }) => {
-        let matchesType = true;
-        let matchesManufacturer = true;
-        let matchesModel = true;
-        let matchesColor = true;
-        let matchesMileage = true;
-        let matchesEngineSpecs = true;
-        let matchesNumSeats = true;
-        let matchesPrice = true;
-
-        if (this.type && this.type !== car.type) {
-          matchesType = false;
-        }
-
-        if (
-          this.manufacturer &&
-          !car.manufacturer
-            .toLowerCase()
-            .includes(this.manufacturer.toLowerCase())
-        ) {
-          matchesManufacturer = false;
-        }
-
-        if (
-          this.model &&
-          !car.model.toLowerCase().includes(this.model.toLowerCase())
-        ) {
-          matchesModel = false;
-        }
-
-        if (
-          this.color &&
-          !car.color.toLowerCase().includes(this.color.toLowerCase())
-        ) {
-          matchesColor = false;
-        }
-
-        if (this.mileage && car.mileage > this.mileage) {
-          matchesMileage = false;
-        }
-
-        if (this.price && car.price > this.price) {
-          matchesPrice = false;
-        }
-
-        return (
-          matchesType &&
-          matchesManufacturer &&
-          matchesModel &&
-          matchesColor &&
-          matchesMileage &&
-          matchesEngineSpecs &&
-          matchesNumSeats &&
-          matchesPrice
-        );
-      }
-    );
-  }
-
-  price!: number;
-  numSeats!: number;
-  numDoors!: number;
-  engineSpecs!: string;
-  model!: string;
-  color!: string;
-  mileage!: number;
-  manufacturer!: string;
-  type!: string;
 }
